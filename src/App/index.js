04b0d4b14b57a09dbe87669661b14a17eb289479
Misael-GC <misael.gomez1623@gmail.com>
2022-07-05 17:11:21 -0500

    Persistencia de datos con Local Storage

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 
-const defaulttodos = [
-  {text:'cortar cebolla', complete: true},
-  {text:'hablar con el prime', complete: false},
-  {text:'salir con una chica', complete: false},
-];
+// const defaulttodos = [
+//   {text:'cortar cebolla', complete: true},
+//   {text:'hablar con el prime', complete: false},
+//   {text:'salir con una chica', complete: false},
+// ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaulttodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if(!localStorageTodos){
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  }else{
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.complete).length;
@@ -26,18 +36,23 @@ function App() {
       return todoText.includes(searchText);
     })
   }
+  const saveTodos = (newTodos)=>{
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
 
    const completeTodo = (text) => { //nueva función de compliteTodos
     const todoIndex = todos.findIndex(todo => todo.text === text); //buscamos su posición que tenga el mismo texto
     const newTodos = [...todos]; //la nueva lista será igual a la primera del estado 
     newTodos[todoIndex].complete = !newTodos[todoIndex].complete;  //ubicando la posición le cambiamos su propiedad a true
-    setTodos(newTodos); //actualizamos el estado.
+    saveTodos(newTodos); //actualizamos el estado.
    }
    const deleteTodo = (text) => { 
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1); //borrara el todo como 1 pastel, ubique en el index y borra 1
-    setTodos(newTodos);
+    saveTodos(newTodos);
    }
 
   return (
