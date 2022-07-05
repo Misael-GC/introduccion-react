674bea7ae3ff2e21173f5d9b3482fd67e2b1e6aa
Misael-GC <misael.gomez1623@gmail.com>
2022-07-05 02:35:46 -0500

    manejo del estado

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch(){
+    const [ searchValue, setSearchValue ] = React.useState(''); //creamos nuestro estado y lo iniciamos con un string vacío
+
     const onSearchValueChange = (event)=>{
         console.log(event.target.value);
+        setSearchValue(event.target.value); //nuestro nuevo estado será lo que escriba el user
     };
 
     return(
@@ -11,9 +14,11 @@ function TodoSearch(){
         <input
         className="TodoSearch"
         placeholder="Filtro"
+        value={searchValue} // el estado que vaya escribiendo el user será el nuevo initial
         onChange={onSearchValueChange}
-        /> 
-        <i class="fa-solid fa-magnifying-glass"></i>
+        />,
+        <i class="fa-solid fa-magnifying-glass"></i>,
+         <p className="texto-extra"> {searchValue} </p> {/*lo ponemos aquí para reafirmar */}
         </div>
     );
 }
